ca8bcc02daa9226c297289fd8417cf2b40ee92d8
Rudi Puchy <rpuchy@gmail.com>
2017-02-20 18:33:31 +1100

    first commit for template analyser

---
+++ Template Analyser/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Template_Analyser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lstbox_wildcards = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Template Location";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(657, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(879, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstbox_wildcards
+            // 
+            this.lstbox_wildcards.FormattingEnabled = true;
+            this.lstbox_wildcards.Location = new System.Drawing.Point(100, 195);
+            this.lstbox_wildcards.Name = "lstbox_wildcards";
+            this.lstbox_wildcards.Size = new System.Drawing.Size(264, 303);
+            this.lstbox_wildcards.TabIndex = 3;
+            this.lstbox_wildcards.SelectedIndexChanged += new System.EventHandler(this.lstbox_wildcards_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Wildcards";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(423, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Analyse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(423, 195);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(752, 457);
+            this.treeView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1256, 675);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstbox_wildcards);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox lstbox_wildcards;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Template Analyser/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace Template_Analyser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            lstbox_wildcards.Items.Clear();
+
+            string[] files = Directory.GetFiles(textBox1.Text);
+
+            List<string> wildcards = new List<string>();
+
+            foreach(string file in files)
+            {
+                string docText = null;
+
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    docText = sr.ReadToEnd();
+                }
+
+                Regex regexText = new Regex("@@(.*?)@@");
+
+                Match m = regexText.Match(docText);
+
+
+                while (m.Success)
+                {
+                    wildcards.Add(m.Value);
+
+                    m = m.NextMatch();
+                }
+            }
+
+            //dedupe the list
+            List<string> deduped = new List<string>(wildcards.Distinct<String>().ToList<string>());
+
+            foreach (var wildcard in deduped)
+            {
+
+                lstbox_wildcards.Items.Add(wildcard);
+            }
+        }
+
+        private void lstbox_wildcards_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string wildcard = lstbox_wildcards.SelectedItem.ToString();
+
+            string[] files = Directory.GetFiles(textBox1.Text);
+
+            treeView1.Nodes.Clear();
+
+            foreach (string file in files)
+            {
+                XmlDocument doc = new XmlDocument();
+
+                doc.Load(file);
+
+                string xpath = "//*[text() = '" + wildcard + "']";
+
+
+                XmlNodeList nodes = doc.SelectNodes(xpath);
+                if (nodes != null)
+                {
+                    foreach (XmlNode node in nodes)
+                    {
+                        //try get a descriptive name
+                        string name = node.ParentNode.SelectSingleNode("Name")?.InnerText;
+
+                        treeView1.Nodes.Add(new TreeNode(node.ParentNode.Name+"-"+name+"-"+node.Name));
+                        TreeNode tNode = new TreeNode();
+                        tNode = treeView1.Nodes[treeView1.Nodes.Count - 1];
+
+                        AddNode(node.ParentNode, tNode);
+                    }
+                }
+
+            }
+
+            //treeView1.ExpandAll();
+
+        }
+
+        private void AddNode(XmlNode inXmlNode, TreeNode inTreeNode)
+        {
+            XmlNode xNode;
+            TreeNode tNode;
+            XmlNodeList nodeList;
+            int i;
+
+            // Loop through the XML nodes until the leaf is reached.
+            // Add the nodes to the TreeView during the looping process.
+            if (inXmlNode.HasChildNodes)
+            {
+                nodeList = inXmlNode.ChildNodes;
+                for (i = 0; i <= nodeList.Count - 1; i++)
+                {
+                    xNode = inXmlNode.ChildNodes[i];
+                    inTreeNode.Nodes.Add(new TreeNode(xNode.Name));
+                    tNode = inTreeNode.Nodes[i];
+                    AddNode(xNode, tNode);
+                }
+            }
+            else
+            {
+                // Here you need to pull the data from the XmlNode based on the
+                // type of node, whether attribute values are required, and so forth.
+                inTreeNode.Text = (inXmlNode.OuterXml).Trim();
+            }
+        }
+
+    }
+}
